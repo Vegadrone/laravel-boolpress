b969186182707d6ec9bf63489f56e3a26c4dc37d
Vegadrone <spenaantonino@gmail.com>
2022-09-29 17:09:49 +0200

    modify router

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,8 +1,25 @@
+//! Importiamo Vue router
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+//! Diciamo alla window(la nostra pagina) che Vue è richesto
+
 window.Vue = require('vue');
 
-import App from './views/App'
+//! Diciamo a Vue di usare VueRouter
 
+Vue.use(VueRouter)
+
+//! Importiamo App dalla cartella js/views e il router
+
+import App from './views/App';
+import router from './router';
+
+//! Nuova istanza di Vue in cui specifichiamo root come collegamento sul blade
+//! render della pagina home su App e specifichaimo il router
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,8 +1,31 @@
-window.Vue = require('vue');
+//!Importiamo VueRouter anche qui, basta solo questo per farlo funzionare
+import VueRouter from 'vue-router';
 
-import App from './views/App'
+//!Importiamo le pagine singole
+import HomePage from './pages/HomePage';
+import ContactsPage from './pages/ContactsPage'; ù
+import AboutPage from './pages/AboutPage';
 
-const app = new Vue({
-    el: '#root',
-    render: h => h(App)
+//!Istanza di VueRouter
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutPage
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: ContactsPage
+        },
+    ],
 });
+
+export default router
